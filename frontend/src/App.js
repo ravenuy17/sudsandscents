c6b5607859a691b137803a64ff1d97b48ae39b94
ravenuy17 <154998021+ravenuy17@users.noreply.github.com>
2024-07-22 01:48:56 +0800

    Screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import CartScreen from './screens/cartScreen';
+import HomeScreen from './screens/homeScreen';
 
+// Define your App component
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/cart">Cart</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/cart" element={<CartScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/adminRoute.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+
+const AdminRoute = ({ children }) => {
+  const userSignIn = useSelector((state) => state.userSignIn);
+  const { userInfo } = userSignIn;
+
+  return userInfo && userInfo.isAdmin ? children : <Navigate to="/signin" />;
+};
+
+export default AdminRoute;
