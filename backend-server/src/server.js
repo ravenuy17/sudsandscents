6c33d399945743ae89b425964fe3ca1b7793cb74
Irvin-Kier <irvinkierpresto26@gmail.com>
2024-07-22 00:36:22 +0800

    Make backend-server dir for express

---
+++ backend-server/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const port = 5000;
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }));
+
+server.get("/", (req, res) => {
+  res.render("Sample");
+});
+
+server.listen(port, () => {
+  console.log(`Server listening at port ${port}`);
+});
